96121a84690382bed0d6df09d887a9a519c5c652
Benjamin Brooks <bjbrooks00@gmail.com>
2023-11-25 08:16:28 -0500

    moved texture to its own files

--- src/main.c
+++ src/main.c
@@ -7,52 +7,7 @@
 #include "l_shader.h"
 #include "l_mesh.h"
 #include "l_camera.h"
-
-// TEXTURE //==================================================================
-
-#include "stb_image.h"
-
-//TODO make a destroy texture func to clean this up
-GLuint lite_gl_texture_create(const char* imageFile){
-	/*create texture*/
-	GLuint texture;
-	glGenTextures(1,&texture);
-	glBindTexture(GL_TEXTURE_2D,texture);
-
-	/*set parameters*/
-	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_S,GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_T,GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
-
-	/*load texture data from file*/
-	int width, height, numChannels;
-	stbi_set_flip_vertically_on_load(true);
-	unsigned char* data = stbi_load(
-			imageFile, &width, &height, &numChannels, 0);
-
-	/*error check*/
-	if (data){
-		if (numChannels == 4){
-			glTexImage2D(
-					GL_TEXTURE_2D,0,GL_RGBA,width,height,0,GL_RGBA,
-					GL_UNSIGNED_BYTE,data);
-			glGenerateMipmap(GL_TEXTURE_2D);
-		} else if (numChannels == 3){
-			glTexImage2D(
-					GL_TEXTURE_2D,0,GL_RGB,width,height,0,GL_RGB,
-					GL_UNSIGNED_BYTE,data);
-			glGenerateMipmap(GL_TEXTURE_2D);
-		}
-	} else {
-		fprintf(stderr,"failed to load texture from '%s'\n", imageFile);
-	}
-
-	/*cleanup*/
-	stbi_image_free(data);
-
-	return texture;
-}
+#include "l_texture.h"
 
 int main (int argc, char* argv[]) {
 	printf("Rev up those fryers!\n");
