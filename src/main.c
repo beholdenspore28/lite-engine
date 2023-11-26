d9bd25e68d939840d27c9fc6794add2d350cf064
Benjamin Brooks <bjbrooks00@gmail.com>
2023-11-26 08:23:11 -0500

    added l_renderer_gl_shader_getMatrixLocation(). + cleanup

--- src/main.c
+++ src/main.c
@@ -3,20 +3,46 @@
 #include <GLFW/glfw3.h>
 #include "l_renderer_gl.h"
 
+GLuint l_renderer_gl_shader_getMatrixLocation(
+		GLuint shader, 
+		const char* uniformName, 
+		blib_mat4_t* m){
+	GLuint MatrixUniformLocation = glGetUniformLocation(
+			shader, 
+			uniformName
+			);
+	if (MatrixUniformLocation >= 0) {
+		glUniformMatrix4fv(
+				MatrixUniformLocation,
+				1,
+				GL_FALSE,
+				&m->elements[0]
+				);
+	}
+	else {
+		fprintf(
+				stderr,
+				"failed to locate matrix uniform. %s %i",
+				__FILE__, 
+				__LINE__
+				);
+	}
+	return MatrixUniformLocation;
+}
+
 int main (int argc, char* argv[]) {
 	printf("Rev up those fryers!\n");
-	l_renderer_gl_runtime runtime = l_renderer_gl_runtime_init();	
 
+	//create stuff
+	l_renderer_gl_runtime runtime = l_renderer_gl_runtime_init();	
 	l_renderer_gl_camera camera = l_renderer_gl_camera_create(85.0f);
 	GLuint shader = l_renderer_gl_shader_create();
-
 	l_renderer_gl_mesh mesh = l_renderer_gl_mesh_createCube();
-
 	l_renderer_gl_transform transform = l_renderer_gl_transform_create();
-
 	blib_mat4_t modelMatrix = l_renderer_gl_transform_GetMatrix(&transform);
-
 	GLuint texture = l_renderer_gl_texture_create("res/textures/test2.png");
+	
+	//texture setup
 	glActiveTexture(GL_TEXTURE0);
 	glBindTexture(GL_TEXTURE_2D, texture);
 
@@ -42,72 +68,25 @@ int main (int argc, char* argv[]) {
 			{
 				glUseProgram(shader);
 				glUniform1i(glGetUniformLocation(shader, "i_texCoord"), 0);
-				GLuint modelMatrixUniformLocation = glGetUniformLocation(
-						shader, 
-						"u_modelMatrix"
+				l_renderer_gl_shader_getMatrixLocation(
+						shader,
+						"u_modelMatrix",
+						&modelMatrix
 						);
-				if (modelMatrixUniformLocation >= 0) {
-					glUniformMatrix4fv(
-							modelMatrixUniformLocation,
-							1,
-							GL_FALSE,
-							&modelMatrix.elements[0]
-							);
-				}
-				else {
-					fprintf(
-							stderr,
-							"failed to locate model matrix uniform. %s %i", 
-							__FILE__, 
-							__LINE__
-							);
-				}
 			}
 
 			//camera update
 			{
-
-				GLuint viewMatrixUniformLocation = glGetUniformLocation(
-						shader, 
-						"u_viewMatrix"
+				l_renderer_gl_shader_getMatrixLocation(
+						shader,
+						"u_viewMatrix",
+						&camera.viewMatrix
 						);
-				if (viewMatrixUniformLocation >= 0) {
-					glUniformMatrix4fv(
-							viewMatrixUniformLocation,
-							1,
-							GL_FALSE,
-							&camera.viewMatrix.elements[0]
-							);
-				}
-				else {
-					fprintf(
-							stderr,
-							"failed to locate view matrix uniform. %s %i", 
-							__FILE__, 
-							__LINE__
-							);
-				}
-
-				GLuint projectionMatrixUniformLocation = glGetUniformLocation(
+				l_renderer_gl_shader_getMatrixLocation(
 						shader, 
-						"u_projectionMatrix"
+						"u_projectionMatrix", 
+						&camera.projectionMatrix
 						);
-				if (projectionMatrixUniformLocation >= 0) {
-					glUniformMatrix4fv(
-							projectionMatrixUniformLocation,
-							1,
-							GL_FALSE,
-							&camera.projectionMatrix.elements[0]
-							);
-				}
-				else {
-					fprintf(
-							stderr,
-							"failed to locate projection matrix uniform. %s %i",
-							__FILE__, 
-							__LINE__
-							);
-				}
 			}
 		}
 
