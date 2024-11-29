bc3cbdf56e23ba97c31604eb9f8f94b2d1dadc02
tauraamui <adampstringer@protonmail.com>
2024-11-29 10:19:53 +0000

    chore: adjust OpenGL version from 4.6 to 4.1

--- src/main.c
+++ src/main.c
@@ -242,7 +242,7 @@ void lite_engine_start(void) {
 	glfwSetErrorCallback(error_callback);
 
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GLFW_TRUE);
 	glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
