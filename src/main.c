020769b287367543fec9e474d763019385989cca
Benjamin Brooks <bjbrooks00@proton.me>
2024-12-26 13:51:26 -0500

    added white space

--- src/main.c
+++ src/main.c
@@ -5,6 +5,7 @@ int main() {
 	//lite_engine_use_render_api(LITE_ENGINE_RENDERER_GL);
 	lite_engine_start();
 	lite_engine_gl_set_prefer_window_position(1920 / 2.0, 1080 / 2.0);
+
 	while (lite_engine_is_running()) {
 		lite_engine_update();
 		//lite_engine_stop();
