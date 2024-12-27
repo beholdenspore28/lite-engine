3d6fc371a5d8ddb61a4ba8a3902c3bb81a3f8558
Benjamin Brooks <bjbrooks00@proton.me>
2024-12-27 02:06:48 -0500

    ECS data reallocation

--- src/lite_engine_ECS.c
+++ src/lite_engine_ECS.c
@@ -6,15 +6,22 @@ int         internal_ECS_entity_capacity      = 1024;
 const int   internal_ECS_entity_growth_factor = 2;
 
 void lite_engine_ECS_start(void) {
+	debug_log("starting ECS...");
 	internal_ECS_data = calloc(sizeof(int), internal_ECS_entity_capacity);
 }
 
 void lite_engine_ECS_stop(void) {
+	debug_log("ECS shutting down...");
 	free(internal_ECS_data);
 }
 
 int lite_engine_ECS_entity_create(void) {
 	internal_ECS_entity_count++;
-	assert(internal_ECS_entity_count < internal_ECS_entity_capacity);
+	if (internal_ECS_entity_count >= internal_ECS_entity_capacity) {
+		internal_ECS_entity_capacity *= 2;
+		internal_ECS_data = realloc(
+				internal_ECS_data,
+				sizeof(int) * internal_ECS_entity_capacity);
+	}
 	return internal_ECS_entity_count;
 }
