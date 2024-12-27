a649e07ab19b0c32635fb463534b5fa96389cc72
Benjamin Brooks <bjbrooks00@proton.me>
2024-12-27 02:47:50 -0500

    code cleanup

--- src/lite_engine_ECS.h
+++ src/lite_engine_ECS.h
@@ -1,8 +1,8 @@
 #ifndef LITE_ENGINE_ECS_H
 #define LITE_ENGINE_ECS_H
 
-void lite_engine_ECS_start(void);
-void lite_engine_ECS_stop(void);
-int  lite_engine_ECS_entity_create(void);
+void lite_engine_ECS_start         (void);
+void lite_engine_ECS_stop          (void);
+int  lite_engine_ECS_entity_create (void);
 
 #endif
