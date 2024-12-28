a6719291e23d644f88119e5e58212355319fefcf
Benjamin Brooks <bjbrooks00@proton.me>
2024-12-28 16:14:52 -0500

    messing with entities

--- src/main.c
+++ src/main.c
@@ -5,36 +5,7 @@ int main() {
 	lite_engine_start();
 
 #if 0 // copied this code from the end of lite_engine_gl_start()
-	const ui64 light = lite_engine_ECS_entity_create();
-	const ui64 camera = lite_engine_ECS_entity_create();
 	const ui64 space_ship = lite_engine_ECS_entity_create();
-
-	internal_object_pool.transforms[light] = (transform_t) {
-		.position = { 0.0, 10, -10 },
-		.rotation = quaternion_identity(),
-		.scale    = vector3_one(1.0),
-	};
-
-	internal_object_pool.lights[light] = (point_light_t) {
-		.diffuse   = vector3_one(0.8f),
-		.specular  = vector3_one(1.0f),
-		.constant  = 1.0f,
-		.linear    = 0.09f,
-		.quadratic = 0.0032f,
-	};
-
-	internal_object_pool.cameras[camera] = (camera_t) {
-		.projection = matrix4_identity(),
-	};
-
-	internal_object_pool.transforms[camera] = (transform_t) {
-		.position = (vector3_t){ 0.0, 0.0, -10.0 },
-		.rotation = quaternion_identity(),
-		.scale    = vector3_one(1.0),
-		.matrix   = matrix4_identity(),
-	};
-
-	lite_engine_gl_set_active_camera(camera);
 
 	internal_object_pool.materials[space_ship] = (material_t) {
 		.shader = lite_engine_gl_shader_create(
@@ -53,8 +24,6 @@ int main() {
 	};
 #endif
 
-	const ui64 light = lite_engine_ECS_entity_create();
-	const ui64 camera = lite_engine_ECS_entity_create();
 	const ui64 space_ship = lite_engine_ECS_entity_create();
 
 	while (lite_engine_is_running()) {
