51aca3538fc2a774d5ae1c1bb06f278e0ae836aa
Benjamin Brooks <bjbrooks00@proton.me>
2024-12-28 23:14:32 -0500

    removed old ECS stuff from main.c

--- src/main.c
+++ src/main.c
@@ -4,26 +4,6 @@ int main() {
 	//lite_engine_use_render_api(LITE_ENGINE_RENDERER_GL);
 	lite_engine_start();
 
-#if 0 // copied this code from the end of lite_engine_gl_start()
-	const ui64 space_ship = lite_engine_ECS_entity_create();
-
-	internal_object_pool.materials[space_ship] = (material_t) {
-		.shader = lite_engine_gl_shader_create(
-				"res/shaders/phong_diffuse_vertex.glsl",
-				"res/shaders/phong_diffuse_fragment.glsl"),
-		.diffuseMap = lite_engine_gl_texture_create("res/textures/test.png"),
-	};
-
-	internal_object_pool.meshes[space_ship] = lite_engine_gl_mesh_lmod_alloc("res/models/cube.lmod");
-	internal_object_pool.meshes[space_ship].enabled = 1;
-
-	internal_object_pool.transforms[space_ship] = (transform_t) {
-		.position = vector3_zero(),
-		.rotation = quaternion_identity(),
-		.scale    = vector3_one(1.0),
-	};
-#endif
-
 	while (lite_engine_is_running()) {
 		lite_engine_update();
 		//lite_engine_stop();
