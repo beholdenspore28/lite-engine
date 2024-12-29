7b2f62e32bd05c7cd54a19117103e7739fb7674e
Benjamin Brooks <bjbrooks00@proton.me>
2024-12-29 08:17:52 -0500

    adding archetypes to ECS

--- src/lite_engine.h
+++ src/lite_engine.h
@@ -16,11 +16,13 @@ typedef uint16_t ui16;
 typedef uint32_t ui32;
 typedef uint64_t ui64;
 
-void   lite_engine_start                     (void);
-void   lite_engine_use_render_api            (ui8 api);
-ui8    lite_engine_is_running                (void);
-void   lite_engine_update                    (void);
-void   lite_engine_stop                      (void);
-double lite_engine_get_time_delta            (void);
+void   lite_engine_start          (void);
+void   lite_engine_use_render_api (ui8 api);
+ui8    lite_engine_is_running     (void);
+void   lite_engine_update         (void);
+void   lite_engine_stop           (void);
+double lite_engine_get_time_delta (void);
+
+ui64   lite_engine_entity_create  (void);
 
 #endif
