95e5c304149c566c8be125b52ea91f59a00e761e
Benjamin Brooks <bjbrooks00@proton.me>
2025-03-27 11:51:05 -0400

    moved OpenAL api to lal.c/h

---
+++ src/lal.h
@@ -0,0 +1,31 @@
+#ifndef LAL_H
+#define LAL_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif // ifdef __cplusplus
+
+#include "lgl.h"
+
+#include <AL/al.h>
+#include <AL/alc.h>
+#include <AL/alut.h>
+
+typedef struct {
+  ALuint id;
+  ALuint buffer;
+  unsigned int count;
+} lal_audio_source_t;
+
+lal_audio_source_t lal_audio_source_alloc(unsigned int count);
+
+void lal_audio_source_update(
+    lgl_object_t       object,
+    lal_audio_source_t source,
+    lgl_context_t      *lgl_context);
+
+#ifdef __cplusplus
+}
+#endif // ifdef __cplusplus
+
+#endif
--- src/main.c
+++ src/main.c
@@ -2,9 +2,7 @@
 #define BLIB_IMPLEMENTATION
 #include "blib/blib_math3d.h"
 
-#include <AL/al.h>
-#include <AL/alc.h>
-#include <AL/alut.h>
+#include "lal.h"
 
 #include <math.h>
 #include <stdio.h>
@@ -102,59 +100,6 @@ void galaxy_generate(
     stars.position[i] = vector3_scale(stars.position[i], radius);
 
   }
-}
-
-typedef struct {
-  ALuint id;
-  ALuint buffer;
-  unsigned int count;
-} lal_audio_source_t;
-
-lal_audio_source_t lal_audio_source_alloc(unsigned int count){
-
-  lal_audio_source_t source;
-
-  source.buffer = 0;
-  alGenBuffers(count, &source.buffer);
-  source.buffer = alutCreateBufferFromFile("res/audio/random (1).wav");
-
-  alGenSources(count,    &source.id);
-  alSourcei(source.id, AL_BUFFER,  source.buffer);
-  alSourcei(source.id, AL_LOOPING, AL_TRUE);
-  alSourcef(source.id, AL_GAIN, 1.0);
-  alSourcePlay(source.id);
-  return source;
-}
-
-void lal_audio_source_update(
-    lgl_object_t       object,
-    lal_audio_source_t source,
-    lgl_context_t      *lgl_context) {
-
-    alSource3f(source.id, AL_POSITION,
-        object.position->x,
-        object.position->y,
-        object.position->z);
-
-    alListener3f(AL_POSITION,
-        lgl_context->camera.position.x,
-        lgl_context->camera.position.y,
-        lgl_context->camera.position.z);
-
-    vector3_t camera_up = vector3_rotate(
-        vector3_up(1.0),
-        lgl_context->camera.rotation);
-
-    float orientation[6] = {
-      lgl_context->camera.position.x,
-      lgl_context->camera.position.y,
-      lgl_context->camera.position.z,
-      camera_up.x,
-      camera_up.y,
-      camera_up.z,
-    };
-
-    alListenerfv(AL_ORIENTATION, orientation);
 }
 
 int main() {
