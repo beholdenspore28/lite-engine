55b11a588ee58904135dfa970efae66ec710453a
Benjamin Brooks <bjbrooks00@proton.me>
2025-04-02 21:31:21 -0400

    removed unnessecary list type

--- src/lgl.c
+++ src/lgl.c
@@ -739,22 +739,16 @@ void lgl_icosphere_mesh_alloc(lgl_batch *batch) {
   batch->render_flags |= LGL_FLAG_INDEXED_DRAW;
 
   {
-    typedef struct {
-      GLuint i0, i1, i2;
-    } face;
-
     enum { indices_count = 60 };
-    const face indices[indices_count] = {
-        {0, 11, 5}, {0, 5, 1},  {0, 1, 7},   {0, 7, 10}, {0, 10, 11},
-        {1, 5, 9},  {5, 11, 4}, {11, 10, 2}, {10, 7, 6}, {7, 1, 8},
-        {3, 9, 4},  {3, 4, 2},  {3, 2, 6},   {3, 6, 8},  {3, 8, 9},
-        {4, 9, 5},  {2, 4, 11}, {6, 2, 10},  {8, 6, 7},  {9, 8, 1},
+    const GLuint indices[indices_count] = {
+        0, 11, 5, 0, 5, 1,  0, 1, 7,   0, 7, 10, 0, 10, 11,
+        1, 5, 9,  5, 11, 4, 11, 10, 2, 10, 7, 6, 7, 1, 8,
+        3, 9, 4,  3, 4, 2,  3, 2, 6,   3, 6, 8,  3, 8, 9,
+        4, 9, 5,  2, 4, 11, 6, 2, 10,  8, 6, 7,  9, 8, 1,
     };
 
     for (unsigned int i = 0; i < indices_count; i++) {
-      list_GLuint_add(&batch->indices, indices[i].i0);
-      list_GLuint_add(&batch->indices, indices[i].i1);
-      list_GLuint_add(&batch->indices, indices[i].i2);
+      list_GLuint_add(&batch->indices, indices[i]);
     }
   }
 
