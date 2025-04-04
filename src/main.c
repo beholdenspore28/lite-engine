d933feaca4e25e6538ba1fafd9f7ecac439a70a2
Benjamin Brooks <bjbrooks00@proton.me>
2025-04-04 19:55:47 -0400

    ignore warning when redefining DEMO macro

--- src/main.c
+++ src/main.c
@@ -1,4 +1,6 @@
 
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wmacro-redefined"
 // DEMOS HERE. move the demo you want to see to the bottom of
 // this stack of define directives then compile the project. it should load your
 // chosen demo scene.
@@ -6,6 +8,7 @@
 #define DEMO demo_cube()      // just a spinning cube with a 3D sound
 #define DEMO demo_physics()   // GPU instancing with dynamic particle system
 #define DEMO demo_galaxy()    // GPU instancing many static objects
+#pragma clang diagnostic pop
 
 #include "lal.h"
 #include "lgl.h"
