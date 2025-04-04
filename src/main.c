627becf75c8618b81fc82d1c51e1ce73a7652dbc
Benjamin Brooks <bjbrooks00@proton.me>
2025-04-04 19:06:41 -0400

    more cleanup in main

--- src/main.c
+++ src/main.c
@@ -2,10 +2,10 @@
 // DEMOS HERE. move the demo you want to see to the bottom of
 // this stack of define directives then compile the project. it should load your
 // chosen demo scene.
-#define DEMO demo_icosphere()// procedural icosphere
-#define DEMO demo_physics()  // GPU instancing with dynamic particle system
-#define DEMO demo_galaxy()   // GPU instancing many static objects
-#define DEMO demo_cube()     // just a spinning textured cube with a 3D sound playing
+#define DEMO demo_icosphere() // procedural icosphere
+#define DEMO demo_galaxy()    // GPU instancing many static objects
+#define DEMO demo_cube()      // just a spinning cube with a 3D sound
+#define DEMO demo_physics()   // GPU instancing with dynamic particle system
 
 #include "lal.h"
 #include "lgl.h"
@@ -17,8 +17,8 @@
 lgl_context *graphics_context;
 lgl_light light;
 
-#include "demo_framebuffer.inc"
 #include "demo_shaders.inc"
+#include "demo_framebuffer.inc"
 #include "demo_flycam.inc"
 #include "demo_cube.inc"
 #include "demo_galaxy.inc"
